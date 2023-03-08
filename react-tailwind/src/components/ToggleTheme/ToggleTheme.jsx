dfb91fd4761efdc5408be102347de3469be36f01
mteusbarbosa <mateus.bm13@gmail.com>
2023-03-08 17:46:43 -0300

    toggle theme component

--- react-tailwind/src/components/ToggleTheme/ToggleTheme.jsx
+++ react-tailwind/src/components/ToggleTheme/ToggleTheme.jsx
@@ -1,11 +1,31 @@
-import { MoonIcon, SunIcon } from "@heroicons/react/24/solid"
+import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
+import { useEffect } from "react";
 
-export default function ToggleTheme(){
-    const toggle = () => {
-        document.documentElement.classList.toggle('dark')
-    }
-    return <div className="hidden sm:block">
-        <MoonIcon className="h-8 text-gray-900 block dark:hidden cursor-pointer" onClick={toggle}></MoonIcon>
-        <SunIcon className="h-8 text-gray-100 hidden dark:block cursor-pointer" onClick={toggle}></SunIcon>
+export default function ToggleTheme() {
+  //Supporting system preference and manual selection
+  const systemPreference = window.matchMedia(
+    "(prefers-color-scheme: dark"
+  ).matches;
+  const pageClasses = document.documentElement.classList;
+
+  useEffect(() => {
+    systemPreference && pageClasses.add("dark");
+  });
+
+  const toggle = () => {
+    document.documentElement.classList.toggle("dark");
+  };
+
+  return (
+    <div className="hidden sm:block">
+      <MoonIcon
+        className="h-8 text-gray-900 block dark:hidden cursor-pointer"
+        onClick={toggle}
+      ></MoonIcon>
+      <SunIcon
+        className="h-8 text-gray-100 hidden dark:block cursor-pointer"
+        onClick={toggle}
+      ></SunIcon>
     </div>
+  );
 }
