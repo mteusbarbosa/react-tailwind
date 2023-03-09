b33f38a51fe1c2935345e6a257ee735690daba4b
mteusbarbosa <mateus.bm13@gmail.com>
2023-03-09 14:25:56 -0300

    add tags

--- react-tailwind/src/components/Article/Article.jsx
+++ react-tailwind/src/components/Article/Article.jsx
@@ -1,12 +1,25 @@
-const Article = ({title, text, tags, image, alt}) => {
-    return <div className="m-5 p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-xl dark:text-gray-200 flex flex-col items-center ">
-        <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold ">{title}</h3>
-        <div className="grid gap-1">
-            {
-                text.map((content, index) => <p className="text-alura-200 dark:text-gray-400" key={index}>{content}</p>)
-            }
-        </div>
+const Article = ({ title, text, tags, image, alt }) => {
+  return (
+    <div className="m-5 p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-xl dark:text-gray-200 flex flex-col items-center ">
+      <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold ">
+        {title}
+      </h3>
+      <div className="w-full flex justify-end gap-2 pr-5 my-2 hidden sm:flex">
+        {tags.map((tag) => (
+          <span key={tag} className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-xs uppercase font-bold">{tag}</span>
+        ))}
+      </div>
+      <div className="grid gap-1">
+        {text.map((content, index) => (
+          <p className="text-alura-200 dark:text-gray-400" key={index}>
+            {content}
+          </p>
+        ))}
+      </div>
+      {image && <img src={image} alt={alt} className="sm:p-4" />}
+      {image && alt && <span className="sr-only">{alt}</span>}
     </div>
-}
+  );
+};
 
 export default Article;
--- react-tailwind/src/components/ArticleList/ArticleList.jsx
+++ react-tailwind/src/components/ArticleList/ArticleList.jsx
@@ -4,7 +4,9 @@ import Article from "../Article/Article";
 export default function ArticleList() {
   return (
     <div className="mt-5 m-auto sm:mt-0 grid gap-5 sm:grid-cols-2 lg:grid-cols-3 max-w-2xl lg:max-w-[1000px]">
-      {data.map((props, index) => <Article key={index} {...props}></Article>)}
+      {data.map((props, index) => (
+        <Article key={index} {...props}></Article>
+      ))}
     </div>
   );
 }
