3686e43ac7e5bb546e2ca24c709e7bde17c2d34a
mteusbarbosa <mateus.bm13@gmail.com>
2023-03-09 17:39:39 -0300

    input invalid and focus

--- react-tailwind/src/components/Article/Article.jsx
+++ react-tailwind/src/components/Article/Article.jsx
@@ -1,12 +1,12 @@
 const Article = ({ title, text, tags, image, alt }) => {
   return (
-    <div className="m-5 p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-xl dark:text-gray-200 flex flex-col items-center ">
+    <div className="m-5 p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-xl dark:text-gray-200 flex flex-col items-center sm:hover:shadow-black sm:hover:shadow-lg sm:hover:dark:shadow-black">
       <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold ">
         {title}
       </h3>
       <div className="w-full flex justify-end gap-2 pr-5 my-2 hidden sm:flex">
         {tags.map((tag) => (
-          <span key={tag} className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-xs uppercase font-bold">{tag}</span>
+          <span key={tag} className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-xs uppercase font-bold hover:bg-alura-200 hover:scale-110">{tag}</span>
         ))}
       </div>
       <div className="grid gap-1">
--- react-tailwind/src/components/Input/Input.jsx
+++ react-tailwind/src/components/Input/Input.jsx
@@ -1,4 +1,8 @@
-export default function Input(props){
-    return <input className="py-1 pl-5 w-full max-w-lg dark:bg-dark-200 text-dark-100 dark:text-gray-200 rounded-full outline-none focus:border focus:border-alura-100" {...props} />
-
+export default function Input(props) {
+  return (
+    <input
+      className="py-1 pl-5 w-full max-w-lg dark:bg-dark-200 text-dark-100 dark:text-gray-200 rounded-full outline-none focus:border-2 focus:border-alura-100 placeholder:text-gray-400 placeholder:dark:text-gray-600 placeholder:text-xs placeholder:uppercase placeholder:font-bold focus:dark:border-gray-600 focus:invalid:border-2 focus:invalid:border-red-700 focus:dark:invalid:border-2 focus:dark:invalid:border-red-700"
+      {...props}
+    />
+  );
 }
