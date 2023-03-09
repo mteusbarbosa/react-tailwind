56f34a76e3f872614702ec36ee125619af81f5e1
mteusbarbosa <mateus.bm13@gmail.com>
2023-03-09 19:19:55 -0300

    cssnano and lamp installed

--- react-tailwind/src/components/Article/Article.jsx
+++ react-tailwind/src/components/Article/Article.jsx
@@ -4,14 +4,14 @@ const Article = ({ title, text, tags, image, alt }) => {
       <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold ">
         {title}
       </h3>
-      <div className="w-full flex justify-end gap-2 pr-5 my-2 hidden sm:flex">
+      <div className="w-full justify-end gap-2 pr-5 my-2 hidden sm:flex">
         {tags.map((tag) => (
           <span key={tag} className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-xs uppercase font-bold hover:bg-alura-200 hover:scale-110">{tag}</span>
         ))}
       </div>
       <div className="grid gap-1">
         {text.map((content, index) => (
-          <p className="text-alura-200 dark:text-gray-400" key={index}>
+          <p className="text-alura-200 dark:text-gray-400 line-clamp-6 sm:line-clamp-none" key={index}>
             {content}
           </p>
         ))}
